8eb807ddfe636ab62b3b4fbb273444186da5c82b
airtoxin <airtoxin@icloud.com>
2016-11-07 01:24:44 +0900

    Sync room

--- templates/src/index.js
+++ templates/src/index.js
@@ -1,10 +1,32 @@
-import React from "react";
-import { render } from "react-dom";
+import React from 'react';
+import firebase from 'firebase';
+import { render } from 'react-dom';
 import { root } from 'baobab-react/higher-order';
 import tree from './tree';
 import ErrorView from './views/ErrorView';
-import App from "./views/pages/App";
+import App from './views/pages/App';
+
+const firebaseApp = firebase.initializeApp({
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  databaseURL: process.env.databaseURL,
+  storageBucket: process.env.storageBucket,
+});
+const db = firebase.database();
+
+const sync = (refPath, selector, defaultValue) => {
+  const ref = db.ref(refPath);
+  const cursor = tree.select(selector);
+
+  ref.on('value', snapshot => {
+    cursor.set(snapshot.val() || defaultValue);
+    tree.commit();
+  });
+  cursor.on('update', updatee => ref.set(updatee.target.get() || defaultValue));
+};
+
+sync('rooms', ['rooms'], {});
 
 const Rooted = root(tree, App);
 
-render(<ErrorView><Rooted /></ErrorView>, document.getElementById("app"));
+render(<ErrorView><Rooted /></ErrorView>, document.getElementById('app'));
--- templates/src/tree.js
+++ templates/src/tree.js
@@ -3,7 +3,8 @@ import Baobab from 'baobab';
 export default new Baobab({
   game: {
     started: false,
-  }
+  },
+  rooms: {},
 }, {
   autoCommit: false,
   asynchronous: true,
